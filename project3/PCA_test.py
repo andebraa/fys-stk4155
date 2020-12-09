8aa092f2a571ebed27b8d7af43efc4284fe77df6
anders <andebraa@uio.no>
2020-12-09 16:29:19 +0100

    pca svm attempt

--- project3/PCA_test.py
+++ project3/PCA_test.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.metrics import plot_confusion_matrix, accuracy_score
 
 def pca_svm(filename):
 
@@ -18,22 +20,13 @@ def pca_svm(filename):
     X_tr, X_te, y_tr, y_te = train_test_split(vectorized, data['label'],test_size = 0.2)
 
 
-    print(np.shape(X_tr))
-    print(np.shape(y_tr))
-    print(np.shape(X_te))
-    print(np.shape(y_te))
-    print(type(X_te))
-    clf.fit(X_tr, y_tr)
-
-    pred_te = clf.predict(X_te)
-    pred_train = clf.predict(X_tr)
-    print("-----------------------------------")
-    print(confusion_matrix(y_te, pred_te))
-    print(classification_report(y_te, pred_te))
-    print(accuracy_score(y_te, pred_te))
-    print("-----------------------------------")
-    print(confusion_matrix(y_tr, pred_train))
-    print(classification_report(y_tr, pred_train))
-    print(accuracy_score(y_tr, pred_train))
-
     pca = PCA(n_components = 69)
+    X_tr = pca.fit_transform(X_tr)
+    X_te = pca.transform(X_te)
+    explained_variance = pca.explained_variance_raio_
+    clf = SVC(kernel = 'linear')
+    clf.fit(X_tr, y_tr)
+    y_pred = clf.predict(X_te)
+
+    plot_confusion_matrix(clf, X_te, y_te)
+    accuracy = accuracy_score(y_te, y_pred)
